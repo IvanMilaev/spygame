a1c70de4d866c4dd6c7079499d1b30debde0e781
ivan_milaev <ivan.milaev@dreamsoft3d.com>
2020-08-03 21:01:12 +0300

    oauth2 oidc setup begin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { CoreModule } from './auth/core/core.module';
 import { AppComponent } from './app.component';
 import { UserProfileComponent } from './containers/user-profile/user-profile.component';
 import { HeaderComponent } from './components/header/header.component';
 
 import { UIShellModule, StructuredListModule } from 'carbon-components-angular';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { ShouldLoginComponent } from './components/should-login/should-login.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UserProfileComponent,
     HeaderComponent,
-    TasksComponent
+    TasksComponent,
+    ShouldLoginComponent
   ],
   imports: [
     BrowserModule,
+    CoreModule.forRoot(),
     AppRoutingModule,
     UIShellModule,
     StructuredListModule
